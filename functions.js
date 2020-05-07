f0b9d3e334a0ddf5354419b0ee123b13fc6bd31d
ichead <51948247+ichead@users.noreply.github.com>
2020-05-06 18:57:47 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function changebg_mouseover(obj) {
+	obj.style.backgroundColor = '#55caf5';
+
+}
+
+function changebg_mouseout(obj) {
+	obj.style.backgroundColor = '#ffffff';
+}
